35c7404b89c80f19d956f760762a22941cc22866
MyModpackIsThebest <95907147+MyModpackIsThebest@users.noreply.github.com>
2021-12-10 16:42:53 +0700

    Add files via upload

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -13,9 +13,7 @@ class CoolUtil
 {
 	// [Difficulty name, Chart file suffix]
 	public static var difficultyStuff:Array<Dynamic> = [
-		['Easy', '-easy'],
-		['Normal', ''],
-		['Hard', '-hard']
+		['Angry', '-angry']
 	];
 
 	public static function difficultyString():String
